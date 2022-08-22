2a38b8de0eb86d6a2877f6e54afe3e5ef9a2a09e
Rajni <rajniyadav263@gmail.com>
2022-08-22 14:39:09 +0530

    Add blogs

---
+++ database/migrations/2022_08_22_081058_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();           
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');                   
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('is_featured')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
